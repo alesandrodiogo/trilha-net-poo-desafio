b34b11a631ff1adc01b37804bcce4725c5544cf8
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2024-09-05 09:50:06 +0100

    solucao do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("930338937","Lumia 900", "3383849404", 32);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine("--------------------------------------");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iphone = new Iphone("996103129","11", "35009337789", 128);
+
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Duolingo");
